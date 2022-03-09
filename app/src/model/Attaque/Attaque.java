de4d5ac81a2718feaa71bc41fe298c77e1e964cd
TOKO nawfel <tokon@informatique.univ-paris-diderot.fr>
2022-03-10 00:21:02 +0100

    Update Attaque.java

--- app/src/model/Attaque/Attaque.java
+++ app/src/model/Attaque/Attaque.java
@@ -1,5 +1,20 @@
+import java.lang.Math;   
+
 public abstract class Attaque {
+    int degats;
+
     public void Attak(Pokemon p ){
-        p.Pdv = p.Pdv - this.Atk;
+        int b = (int)(Math.random()*(50-p.Crit)+1); 
+        if(b == 50 ){
+      degats =  p.getDef - this.getAtk*2;
+       }else{
+            degats =  p.getDef - this.getAtk;
+       }
+            if(degats < 0){
+                p.setPdv (p.getPdv + degats);
+          
+
+        }
     }
 }
+
