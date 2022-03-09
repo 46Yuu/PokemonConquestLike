fa39e38facfc5b878bcc973a3cf313113c77c64b
leninive <adrien.leniniven@gmail.com>
2022-03-09 20:20:51 +0100

    FIX bug affichage bouton et nouvelle classe PanelBoutons

--- app/src/main/java/pokemon/vue/Vue.java
+++ app/src/main/java/pokemon/vue/Vue.java
@@ -18,7 +18,6 @@ public class Vue extends JFrame{
 	private Controleur controleur;
 	public Tile[][] arrayTile;
 	public JButton buttonCommencer=new JButton("Jouer");
-	private JButton boutonAttaquer = new JButton("Attaquer");
 	
 	public Vue(Controleur c) {
 		controleur=c;
@@ -32,21 +31,19 @@ public class Vue extends JFrame{
 		buttonCommencer.addActionListener( event -> {
 			JPanel contentPane=new JPanel();
 			setContentPane(contentPane);
-		
+
 			contentPane.setLayout(new GridLayout(0,2));
 			panelTerrain.setBackground(Color.black);
 			panelInfos.setBackground(Color.green);
 			panelInfos.setLayout(new GridLayout(2,0));
-			panelBoutons.setLayout(new FlowLayout());
 			panelJoueurs.setBackground(Color.red);
-			panelBoutons.setBackground(Color.GRAY);
+			PanelBoutons panelBoutons=new PanelBoutons();
+			
 			panelInfos.add(panelJoueurs);
 			panelInfos.add(panelBoutons);
 			panelJoueurs.add(labelJoueur);
 			contentPane.add(panelInfos);
 			contentPane.add(panelTerrain);
-
-			boutonAttaquer.add(panelBoutons);
 
 			panelTerrain.setLayout(new GridLayout(controleur.getHeight(),controleur.getWidth(),1,1));
 			for(int i=0; i<controleur.getHeight(); i++){
@@ -70,7 +67,7 @@ public class Vue extends JFrame{
 	 */
 	public void miseAJourInformations(String joueur) {
 		labelJoueur.setText("Tour du joueur : " + joueur );
-	} 
+	}
 
 	/**
 	 * sélectionne tous les tiles dont les coordonnées se trouvent dans la liste listPaires
