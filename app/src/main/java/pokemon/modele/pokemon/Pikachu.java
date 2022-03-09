bed609aeee9884160eb247b5089a4998a4a323a5
Tran Henri Bergson <henri-bergson.tran@etu.u-paris.fr>
2022-03-09 20:33:32 +0100

    ajout liste d'attaque pokemon et choix v1

--- app/src/main/java/pokemon/modele/pokemon/Pikachu.java
+++ app/src/main/java/pokemon/modele/pokemon/Pikachu.java
@@ -3,5 +3,8 @@ package pokemon.modele.pokemon;
 public class Pikachu extends Pokemon {
     public Pikachu(int p, int a, String t) {
         super("Pikachu",3,p, a, t, "src/main/resources/pikachu.png");
+        this.addAttaqueListe("Eclair", "Electrique");
+        this.addAttaqueListe("Vive-Attaque", "Normal");
+        this.addAttaqueListe("Double Pied", "Combat");
     }
 }
