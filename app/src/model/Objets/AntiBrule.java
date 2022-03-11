4dcb7fe1778e517ad53433c269536a704fdc076a
Wissam Ghanem <Wissam.ghanem@outlook.fr>
2022-03-11 13:16:28 +0100

    k

--- app/src/model/Objets/AntiBrule.java
+++ app/src/model/Objets/AntiBrule.java
@@ -1,3 +1,11 @@
 public class AntiBrule extends Objets{
-    
+    public AntiBrule(){
+        this.nom="Anti Brule";
+    }
+
+    public void Utiliser(Pokemon p){
+        if(p.etat == "brule"){
+            p.etat ="";
+        }
+   }
 }
